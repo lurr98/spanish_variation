5dfc8b81af740108f8f6e2fdc3a851cf42a49db2
lurr98 <laurazeidler.konstanz@gmail.com>
2024-02-13 18:24:02 +0100

    started scripts for data analysis

---
+++ corpus/corpus_analysis.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Author: Laura Zeidler
+Last changed: 13.02.2024
+
+**description**
+"""
+
+def length_of_paragrpahs():
+
+    ##
+
+
+def most_frequent_ngram():
+    # not only unigrams!
+
+    ##
+
+
+def most_frequent_pos():
+    # not only unigrams!
+
+    ##
+
+
+def chi_square():
+
+    ##
+
+
+def wilcoxon_ranks_test():
+
+    ##
+
+
+def mutual_information():
+
+    ##  
+
+
+def log_likelihood():
+
+    ##
+
+
+def fishers_exact_text():
+
+    ##
---
+++ corpus/corpus_reader.py
@@ -0,0 +1,7 @@
+# TODO: define class of CorpusReader which can
+#
+#       - read the data for a specific class of the corpus
+#           - take into account that the separate classes are split into multiple files as well 
+#       - read the data for all desired classes at once
+#       - split the data in paragraphs
+#       - preprocess the data ?
